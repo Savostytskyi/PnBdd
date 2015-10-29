f971e6f62481373eec234a76ba60ea44726980a8
Anton Savostytskyi <Anton_savostytskyi@epam.com>
2015-10-29 13:55:01 +0200

    BDD v1.0.2

--- src/main/java/pn/com/ua/pages/PnHomePage.java
+++ src/main/java/pn/com/ua/pages/PnHomePage.java
@@ -16,8 +16,6 @@ public class PnHomePage extends PageObject {
 
   public static final String CATEGORIES = "//a[@class='main_page_link_catalog']";
   public static final String SUB_CATEGORIES = "//div[contains(@class, 'ct ')]/a";
-  public static final String SUB_CATEGORIES_BY_TEXT = SUB_CATEGORIES + "[text()='%s']";
-  public static final String CATEGORY_BY_TEXT = "//a[text()='%s']";
   public static final String SEARCH_FIELD = "//input[@id='edit-name-1']";
   public static final String SEARCH_BUTTON = "//input[@id='edit-submit-1']";
 
@@ -27,14 +25,13 @@ public class PnHomePage extends PageObject {
   @FindBy(xpath = SUB_CATEGORIES)
   private List<WebElementFacade> subcategories;
 
+
   public void selectCategory (String categoryName) {
-    waitForPresenceOf(String.format(CATEGORY_BY_TEXT, categoryName));
-    find(By.xpath(String.format(CATEGORY_BY_TEXT, categoryName))).click();
+    find(By.linkText(categoryName)).click();
   }
 
   public void selectSubCategory (String subCategoryName) {
-    waitForPresenceOf(String.format(SUB_CATEGORIES_BY_TEXT, subCategoryName));
-    find(By.xpath(String.format(CATEGORY_BY_TEXT, subCategoryName))).click();
+    find(By.linkText(subCategoryName)).click();
   }
 
   public void searchOnHomePage(String productName) {
